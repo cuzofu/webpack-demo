85794164754d41e410d3b948e3d81ccafc52aa53
cuzofu <cuzofu@qq.com>
2017-07-31 23:06:02 +0800

    update

---
+++ webpack.dev.js
@@ -0,0 +1,20 @@
+module.exports = {
+  devtool: 'cheap-module-source-map',
+
+  output: {
+    path: path.join(__dirname, '/../dist/assets'),
+    filename: '[name].bundle.js',
+    publicPath: publicPath,
+    sourceMapFileName: '[name].map'
+  },
+
+  devServer: {
+    port: 7777,
+    host: 'localhost',
+    historyApiFallback: true,
+    noInfo: false,
+    stats: 'minimal',
+    publicPath: publicPath
+  }
+
+}
